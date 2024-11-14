2c90c880d484d54aa9aef0448cc5e7acce8c28bd
Adaśko <ak@gmail.com>
2024-11-14 22:46:18 +0100

    nowe endpoint'y

--- public/index.php
+++ public/index.php
@@ -5,6 +5,11 @@ use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
+$templateDir = __DIR__ . '/../templates';
+$compileDir = __DIR__ . '/templates_c';
+
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+
 $app->get('/', function ($request, $response) {
     $smarty = new \Smarty();
     $smarty->setTemplateDir(__DIR__ . '/../templates');
@@ -25,6 +30,40 @@ $app->get('/hello/{name}', function ($request, $response, $args) {
 
     // Renderowanie szablonu hello.tpl
     $response->getBody()->write($smarty->fetch('hello.tpl'));
+    return $response;
+});
+
+// Nowy endpoint /test
+$app->get('/test', function ($request, $response) use ($templateDir, $compileDir) {
+    $smarty = new \Smarty();
+    $smarty->setTemplateDir(__DIR__ . '/../templates');
+    $smarty->setCompileDir(__DIR__ . '/templates_c');
+
+    // Przykładowe dane, które można przekazać do szablonu test.tpl
+    $smarty->assign('message', 'To jest testowy endpoint /test.');
+
+    // Renderowanie szablonu test.tpl
+    $response->getBody()->write($smarty->fetch('test.tpl'));
+    return $response;
+});
+
+// Nowy endpoint POST /update-video
+$app->post('/upload-video', function ($request, $response) use ($templateDir, $compileDir) {
+    $smarty = new \Smarty();
+    $smarty->setTemplateDir($templateDir);
+    $smarty->setCompileDir($compileDir);
+
+    // Pobranie danych POST z requestu
+    $data = $request->getParsedBody();
+    $chunkIndex = htmlspecialchars($data['chunkIndex'] ?? '');  // Dla bezpieczeństwa użyj htmlspecialchars
+    $totalChunks = htmlspecialchars($data['totalChunks'] ?? '');
+
+    // Przypisanie danych do szablonu Smarty
+    $smarty->assign('chunkIndex', $chunkIndex);
+    $smarty->assign('totalChunks', $totalChunks);
+
+    // Renderowanie szablonu update-video.tpl z przekazanymi danymi
+    $response->getBody()->write($smarty->fetch('update-video.tpl'));
     return $response;
 });
 
---
+++ templates/update-video.tpl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Wideo zaktualizowane</title>
+</head>
+<body>
+<h1>Zaktualizowano Wideo</h1>
+<p><strong>chunkIndex:</strong> {$chunkIndex}</p>
+<p><strong>totalChunks:</strong> {$totalChunks}</p>
+<p>Dziękujemy za aktualizację wideo.</p>
+</body>
+</html>
